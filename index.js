d62579d09b33856c5641807850a8dc5032924f8b
Rahmatullaah Boluwatife Ibraheem <ibraheemrahmatullah@gmail.com>
2021-04-03 21:24:26 +0100

    day 1
    Array basics

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+
+
+let firstArray = ["I", "Need", "To", "Get", "This", "Right"];
+
+let domText = firstArray.join(' ');
+
+console.log(domText);
+
+document.getElementById("getme").innerHTML= domText;
+
+   
+// showMe.innerHTML= domText;
+    
+
+
+
+
+
+
+
+let secondArray = 'Me,JavaScript,A match.';
+
+let domTexttwo = secondArray.split(' ');
+
+console.log(domTexttwo);
+
+
+
