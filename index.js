c05930dbdfddfe670332b985d3e2902579eff6b4
Rahmatullaah Boluwatife Ibraheem <ibraheemrahmatullah@gmail.com>
2021-04-18 20:26:40 +0100

    day 2 still giving me the bugzzzzzzzzz

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ console.log(domText);
 document.getElementById("getme").innerHTML= domText;
 
 
-let secondArray = 'Me,JavaScript,A match.';
+let secondArray = 'Me,JavaScript,A,match.';
 
 let domTexttwo = secondArray.split(' ');
 
 console.log(domTexttwo);
 
+
+//No.2 - nesting and accessing nested arrays
 
 let foodArr = [
     ["rice", "chicken", "soup"],
@@ -20,10 +22,33 @@ let foodArr = [
     ["fries", "salad", "steak"],
     [['noodles', 'chicken'], "pasta", "fish"]
   ];
+let weirdFood =  [foodArr[0][2], "and", foodArr[1][2]];
 
-let weirdFoodCombo = {
+let weirdFoodCombo = weirdFood.join(' ');
+
+document.getElementById("getmetoo").innerHTML= weirdFoodCombo;
+
+let foodbtn = document.getElementById("foodButton");
+
+//  _foodFunction=>{
+ 
+//   document.getElementById("getmetoo").classList.add('showFood');
+
+// }
+
+foodbtn.addEventListener('click', () => {
+  document.getElementById("getmetoo").classList.add('showFood')
+})
+
+
+
+
+console.log(weirdFood);
+console.log(weirdFoodCombo);
     
-}
+
+
+
   
 
 
